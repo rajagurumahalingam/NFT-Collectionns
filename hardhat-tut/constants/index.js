1053c6cbc286bc6cb9808e3f4c3ea7edb27ed909
Rajaguru Mahalingam <mrguru1115@gmail.com>
2022-02-10 12:38:29 +0530

    NFT collection

---
+++ hardhat-tut/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x7486346179495430Df41B3ADD2787347A4dA853B";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
